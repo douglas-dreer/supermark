929e400ad92cf5b91aef77368224e5437707e34a
Douglas Dreer <douglasdreer@outlook.com.br>
2025-09-17 20:34:08 -0300

    feat: commit inicial

---
+++ src/main/java/br/com/supermark/SupermarkApplication.java
@@ -0,0 +1,13 @@
+package br.com.supermark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermarkApplication.class, args);
+	}
+
+}
